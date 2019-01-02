330efdb4922a38e50cc56d47e88ae9eab19539ec
De La Cruz <axd170005@utdallas.edu>
2019-01-02 16:54:35 -0600

    Commit for the new Year - minor changes to the UI

--- src/app/home/home-info.component.ts
+++ src/app/home/home-info.component.ts
@@ -9,13 +9,13 @@ import { Component, OnInit } from '@angular/core';
 
 
 export class HomeInfoComponent implements OnInit {
-  Description = 'Welcome to my webpage. It was designed and developed in Angular and Materializecss with Firebase in the backend';
+  Description = "Welcome to my webpage. It was designed and developed in Angular and Materializecss using Google's Firebase in the backend";
   Bio = "Andre De La Cruz graduated from the University of Texas at El Paso in 2013 with a degree in Computer Science. "
-      + "His area of focus includes web developing single page apps (SPA) in Angular and HTML5 in his current position among other duties such report analysis and administrative responsibilities. " 
-      + "However, he practices and enjoys other programming languages such as C# and Java. " 
-      +  "A long term goal is to secure a position as a full time software or web developer combining Angular and C#. "
-      +  "Andre has experience in programming in C, SQL, and Python. 'I would like my career path to include programming C# and ASP.NET while enhancing my understanding in web development. "
-      +  "But I do enjoy creating web apps and challanges that come with it.' "
+      + "His area of focus includes web developing single page apps (SPA) in Angular and HTML5.  Other current responsibilities include data analysis and technical support. " 
+      + "However, he practices and enjoys writing code in C# and Java. " 
+      +  "A long term goal is to secure a position as a full time software or web developer combining Angular, C#, and SQL. "
+      +  "Andre has programmed with various languages such as C, SQL, Python, TypeScript. I have knowledge of Firebase and AngularFire.  'I would like my career path to include programming  while enhancing my understanding in web development. "
+      +  "'I do enjoy programming and the challanges that come with it.' "
   constructor() {}
 
     ngOnInit() {}
